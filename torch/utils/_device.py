286bad1495d9732afc74eae3d367dd487639d330
Michael Lazos <mlazos@meta.com>
2024-08-16 16:42:39 -0700

    Fix DeviceContext bug
    ghstack-source-id: 6e0da25e3ed932eb49a8d59b4d7d4975beda9b7c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133729

--- torch/utils/_device.py
+++ torch/utils/_device.py
@@ -1,8 +1,9 @@
 # mypy: allow-untyped-defs
 from typing import Optional
 import torch
-from torch.overrides import TorchFunctionMode
+from torch.overrides import TorchFunctionMode, _pop_mode, _push_mode
 from torch.utils._contextlib import context_decorator
+from torch._C import _len_torch_function_stack
 import functools
 
 CURRENT_DEVICE: Optional[torch.device] = None
@@ -65,12 +66,36 @@ class DeviceContext(TorchFunctionMode):
         global CURRENT_DEVICE
         self.old_device = CURRENT_DEVICE
         CURRENT_DEVICE = self.device
-        return super().__enter__()
+        # We need to put the device at the bottom of the stack
+        # If we set default device within a function mode context
+        # exiting that context mode will pop the device function mode off
+        # of the stack incorrectly
+        cur_stack = []
+        for _ in range(_len_torch_function_stack()):
+            cur_stack.append(_pop_mode())
+
+        _push_mode(self)
+
+        for mode in reversed(cur_stack):
+            _push_mode(mode)
+
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         global CURRENT_DEVICE
         CURRENT_DEVICE = self.old_device
-        return super().__exit__(exc_type, exc_val, exc_tb)
+        cur_stack = []
+        # Invariant: there should only be one DeviceContext on the stack at any time
+        # search the stack for the device context and pop it, restore previous stack
+        # entries popped while searching
+        for _ in range(_len_torch_function_stack()):
+            mode = _pop_mode()
+            if mode is self:
+                break
+            else:
+                cur_stack.append(mode)
+
+        for mode in reversed(cur_stack):
+            _push_mode(mode)
 
     def __torch_function__(self, func, types, args=(), kwargs=None):
         kwargs = kwargs or {}
